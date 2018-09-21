6aedfec86680a983013d2424e010d673f7099f68
Mattias Ormestad <mattias.ormestad@gmail.com>
2018-09-21 12:00:31 +0200

    Added template config file

---
+++ lib/config_template.php
@@ -0,0 +1,47 @@
+<?php
+// Please remove "_template" from file name after adding necessary credentials
+
+$CONFIG=array(
+	'site' => array(
+		'name'		=> '', 
+		'url'		=> '', 
+		'subdir'	=> '', 
+		'admin'		=> ''
+	), 
+	'mysql' => array(
+		'user' 		=> '', 
+		'pass' 		=> '', 
+		'db' 		=> '',
+		'server'	=> ''
+	), 
+	'clarity' => array(
+		'user' 		=> '', 
+		'pass' 		=> '', 
+		'uri'		=> ''
+	), 
+	'couch' => array(
+		'user' 		=> '', 
+		'pass' 		=> '', 
+		'host'		=> '', 
+		'port'		=> , 
+		'views'		=> array(
+			'users'		=> '', 
+			'projects'	=> ''
+		)
+	), 
+	'portal' => array(
+		'user' 		=> '', 
+		'pass' 		=> '', 
+		'host'		=> ''
+	), 
+	'uservalidation' => array(
+		'allowed'	=> FALSE, 
+		'salt'		=> '', 
+		'roles'		=> array('Unregistered','User','Manager','Administrator'),
+		'status'	=> array('Blocked','Unconfirmed','Active')
+	), 
+	'publications' => array(
+		'keywords'	=> array(
+		)
+	)
+);
